d273c34921c6fc22151936061025ec5a61346fff
murilothebr <murilothebr@hotmail.com>
2024-02-22 23:18:10 -0300

    Project/ feat: temporary docker-compose to run mongodb

--- ProductApi/Program.cs
+++ ProductApi/Program.cs
@@ -1,12 +1,24 @@
+using ProductApi.Models;
+using ProductApi.Services;
+using ProductApi.Services.Interfaces;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.Configure<ProductDatabaseSettings>(
+    builder.Configuration.GetSection("ProductDatabase"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
