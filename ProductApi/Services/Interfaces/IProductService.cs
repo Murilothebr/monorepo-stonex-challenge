64def078501fc91d621a9fd7ff2b20b40bf04ea5
murilothebr <murilothebr@hotmail.com>
2024-02-22 23:16:47 -0300

    backend/ feat: Create first service to run Product Crud operations

---
+++ ProductApi/Services/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Services.Interfaces;
+
+public interface IProductService
+{
+    Task<List<Product>> GetAllAsync();
+
+    Task<Product?> GetAsync(string id);
+
+    Task CreateAsync(Product newProduct);
+
+    Task UpdateAsync(string id, Product updatedProduct);
+
+    Task RemoveAsync(string productId);
+}
