26e0631816c382fdd9465e04a3c5db97101c0bd8
murilothebr <murilothebr@hotmail.com>
2024-02-22 23:17:17 -0300

    backend/ feat: First controller with Product crud operations

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+using ProductApi.Services.Interfaces;
+
+namespace ProductApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController
+{
+    private readonly IProductService _productService;
+
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Product>> GetProduct()
+    {
+        return await _productService.GetAllAsync();
+    }
+}
