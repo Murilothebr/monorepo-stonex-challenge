8f5f00d7048afaad802beb201a82ae6566ed6e50
murilothebr <murilothebr@hotmail.com>
2024-02-25 10:15:17 -0300

    frontend / feat: new header and card component, and new logo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { HeaderMegaMenu } from '../components/HeaderMenu';
+import { BadgeCard } from "../components/ProductCard";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
-    </main>
+    <div>
+    <HeaderMegaMenu />
+    <BadgeCard />
+    </div>
   );
 }
