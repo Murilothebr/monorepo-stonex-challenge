0cdf77a0efb5c112580d7b78578e2776b34e0aea
murilothebr <murilothebr@hotmail.com>
2024-02-26 22:47:48 -0300

    frontenc / feat: new color to form validation

--- frontend/src/app/CreateProduct/page.tsx
+++ frontend/src/app/CreateProduct/page.tsx
@@ -1,84 +1,89 @@
 "use client"
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Box, Button, Group, TextInput } from "@mantine/core";
+import { Box, Button, Group, TextInput, rem } from "@mantine/core";
 import { useForm } from 'react-hook-form';
 import { z } from "zod";
 import { PostCard } from '../../services/apiService';
 
 interface CardData {
- name: string;
- sku: string;
- price: string;
- stock: string;
- description: string;
- imageUrls: string[];
- tags: string[];
- sessions: string[];
- productId: string;
- inStock: boolean;
+  name: string;
+  sku: string;
+  price: string;
+  stock: string;
+  description: string;
+  imageUrls: string[];
+  tags: string[];
+  sessions: string[];
+  productId: string;
+  inStock: boolean;
 }
 
 const productSchema = z.object({
   name: z.string().min(1, 'Name is required'),
   sku: z.string().min(5).max(20, 'SKU must be between 5 and 20 characters'),
   price: z.string().regex(/^\d+(\.\d{1,2})?$/, 'Price must be a valid number'),
-  stock: z.string().regex(/^\d+$/, 'Stock must be a valid number'),
+  stock: z.string().regex(/^\d+$/, 'Stock must be a valid number'),
   description: z.string().min(1, 'Description is required'),
-  imageUrls:  z.string().min(1, 'At least one Image URL is required'),
-  tags:  z.string().min(1, 'At least one tag is required'),
+  imageUrls: z.string().min(1, 'At least one Image URL is required'),
+  tags: z.string().min(1, 'At least one tag is required'),
   sessions: z.string().min(1, 'At least one session is required'),
 });
 
 export default function CreateProduct() {
- const { register, handleSubmit, formState: { errors } } = useForm({
-  resolver: zodResolver(productSchema)
- });
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(productSchema)
+  });
 
- const onSubmit = (data: any) => {
-  
+  const onSubmit = (data: any) => {
+    const convertedData: CardData = {
+      name: data.name,
+      sku: data.sku,
+      price: data.price,
+      stock: data.stock,
+      description: data.description,
+      imageUrls: data.imageUrls ? [data.imageUrls] : [],
+      tags: data.tags ? [data.tags] : [],
+      sessions: data.sessions ? [data.sessions] : [],
+      productId: "",
+      inStock: true
+    };
 
-  const convertedData: CardData = {
-    name: data.name,
-    sku: data.sku,
-    price: data.price,
-    stock: data.stock,
-    description: data.description,
-    imageUrls: data.imageUrls ? [data.imageUrls] : [],
-    tags: data.tags ? [data.tags] : [],
-    sessions: data.sessions ? [data.sessions] : [],
-    productId: "",
-    inStock: true
+    console.log(data);
+
+    try {
+      PostCard(convertedData);
+      alert("sucess")
+    }
+    catch (error) {
+      alert("error")
+    }
   };
 
-  console.log(data);
-  PostCard(convertedData);
- };
+  return (
 
- return (
-  
-  <Box maw={340} mx="auto">
-   <form onSubmit={handleSubmit(onSubmit)}>
-    <TextInput mt={20} label="Name" placeholder="Name" {...register('name')} />
-    {errors.name && <p style={{color: "red"}}>{errors.name.message?.toString()}</p>}
-    <TextInput mt={20} label="Sku" placeholder="Sku" {...register('sku')} />
-    {errors.sku && <p style={{color: "red"}}>{errors.sku.message?.toString()}</p>}
-    <TextInput mt={20} type="number" label="Price" placeholder="Price" {...register('price')} />
-    {errors.price && <p style={{color: "red"}}>{errors.price.message?.toString()}</p>}
-    <TextInput mt={20} type="number" label="Stock" placeholder="Stock" {...register('stock')} />
-    {errors.stock && <p style={{color: "red"}}>{errors.stock.message?.toString()}</p>}
-    <TextInput mt={20} label="Description" placeholder="Description" {...register('description')} />
-    {errors.description && <p style={{color: "red"}}>{errors.description.message?.toString()}</p>}
-    <TextInput mt={20} label="Image Url" placeholder="Image Url" {...register('imageUrls')} />
-    {errors.imageUrls && <p style={{color: "red"}}>{errors.imageUrls.message?.toString()}</p>}
-    <TextInput mt={20} label="Tags" placeholder="Tags" {...register('tags')} />
-    {errors.tags && <p style={{color: "red"}}>{errors.tags.message?.toString()}</p>}
-    <TextInput mt={20} label="Sessions" placeholder="Sessions" {...register('sessions')} />
-    {errors.sessions && <p style={{color: "red"}}>{errors.sessions.message?.toString()}</p>}
-    <Group justify="center" mt="xl">
-     <Button type="submit">Submit Product !</Button>
-    </Group>
-   </form>
-  </Box>
- );
+    <Box maw={340} mx="auto">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextInput mt={20} label="Name" placeholder="Name" {...register('name')} />
+        {errors.name && <p style={{ color: "red" }}>{errors.name.message?.toString()}</p>}
+        <TextInput mt={20} label="Sku" placeholder="Sku" {...register('sku')} />
+        {errors.sku && <p style={{ color: "red" }}>{errors.sku.message?.toString()}</p>}
+        <TextInput mt={20} type="number" label="Price" placeholder="Price" {...register('price')} />
+        {errors.price && <p style={{ color: "red" }}>{errors.price.message?.toString()}</p>}
+        <TextInput mt={20} type="number" label="Stock" placeholder="Stock" {...register('stock')} />
+        {errors.stock && <p style={{ color: "red" }}>{errors.stock.message?.toString()}</p>}
+        <TextInput mt={20} label="Description" placeholder="Description" {...register('description')} />
+        {errors.description && <p style={{ color: "red" }}>{errors.description.message?.toString()}</p>}
+        <TextInput mt={20} label="Image Url" placeholder="Image Url" {...register('imageUrls')} />
+        {errors.imageUrls && <p style={{ color: "red" }}>{errors.imageUrls.message?.toString()}</p>}
+        <TextInput mt={20} label="Tags" placeholder="Tags" {...register('tags')} />
+        {errors.tags && <p style={{ color: "red" }}>{errors.tags.message?.toString()}</p>}
+        <TextInput mt={20} label="Sessions" placeholder="Sessions" {...register('sessions')} />
+        {errors.sessions && <p style={{ color: "red" }}>{errors.sessions.message?.toString()}</p>}
+        <Group justify="center" mt="xl">
+          <Button type="submit">Submit Product !</Button>
+        </Group>
+      </form>
+    </Box>
+  );
 }
